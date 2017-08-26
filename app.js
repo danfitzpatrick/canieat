db0ba65418f5af3945e4951ab760703632e9b090
Dan Fitzpatrick <dan.fpatrick@gmail.com>
2017-08-26 07:21:23 -0700

    Complete basic functionality

--- app.js
+++ app.js
@@ -1,148 +1,115 @@
 'use strict';
-// get the url
+var G = {};
 var url = window.location.href;
-//getting the access token from url
 var access_token = url.split("#")[1].split("=")[1].split("&")[0];
-// get the userid
 var userId = url.split("#")[1].split("=")[2].split("&")[0];
 
-var caloriesBMR = ""
+// get BMR calories so far for today
+function minsToMidnight() {
+  var now = new Date();
+  var then = new Date(now);
+  then.setHours(24, 0, 0, 0);
+  return (then - now) / 6e4;
+}
+G.minsToMidnight = minsToMidnight();
 
+// yesterday
 var date = new Date();
 date.setDate(date.getDate() - 1);
-var yesterday = formatDate(date);
-
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
-
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
-
     return [year, month, day].join('-');
 }
+G.yesterday = formatDate(date);
 
-function yourFunction(callback) {
+$.when (
+
+  $.ajax({ // activities/calories ... includes tracker, bmr, manually-logged activity
+    type: 'GET',
+    beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+    url: "https://api.fitbit.com/1/user/"+userId+"/activities/calories/date/today/1d.json",
+    success:function(data, status, xhr){
+      // console.log(data);
+      G.tracker_bmr_logged_calories = parseInt(data['activities-calories'][0]['value']);
+        //        G.BMR_by_minute = bmr / ( 24 * 60 );
+      }
+  }),
+
+  $.ajax({ // activities/caloriesBMR - Only BMR calories.
+    type: 'GET',
+    beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+    url: "https://api.fitbit.com/1/user/"+userId+"/activities/caloriesBMR/date/" + G.yesterday + "/1d.json",
+    success:function(data, status, xhr){
+      // console.log(data);
+      var bmr = G.BMR_total_yesterday = parseInt(data['activities-caloriesBMR'][0]['value']);
+                G.BMR_by_minute = bmr / ( 24 * 60 );
+      }
+  }),
+
+      $.ajax({ //
+        type: 'GET',
+        beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+        url: "https://api.fitbit.com/1/user/"+userId+"/activities/activityCalories/date/today/1d.json",
+        success:function(data, status, xhr){
+        //  console.log(data);
+          G.activity_calories = parseInt(data['activities-activityCalories'][0]['value']);
+          }
+      }),
+
+          $.ajax({ //activities/tracker/calories - Calories burned inclusive of BMR according to movement captured by a Fitbit tracker.
+            type: 'GET',
+            beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+            url: "https://api.fitbit.com/1/user/"+userId+"/activities/tracker/activityCalories/date/today/1d.json",
+            success:function(data, status, xhr){
+          //    console.log(data);
+
+              G.tracker_activity_calories = parseInt(data['activities-tracker-activityCalories'][0]['value']);
+              }
+          }),
+
+          $.ajax({ //activities/tracker/calories - Calories burned inclusive of BMR according to movement captured by a Fitbit tracker.
+            type: 'GET',
+            beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+            url: "https://api.fitbit.com/1/user/"+userId+"/foods/log/caloriesIn/date/today/1d.json",
+            success:function(data, status, xhr){
+              console.log(data);
+
+              G.caloriesIn = parseInt(data['foods-log-caloriesIn'][0]['value']);
+              }
+          }),
+
     $.ajax({ //activities/tracker/calories - Calories burned inclusive of BMR according to movement captured by a Fitbit tracker.
       type: 'GET',
-      beforeSend: function(request) {
-         request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-      },
-        url: "https://api.fitbit.com/1/user/"+userId+"/activities/tracker/calories/date/today/1d.json",
-        success:function(data, status, xhr){
-        	//var trackerCalories = data['activities-tracker-calories'][0]['value'];
-          //console.log('trackerCalories = ' + trackerCalories);
-          //$('#calories-left').html(trackerCalories);
-              //function myClosure(trackerCalories){
-                //   return trackerCalories;
-            //  }
-
+      beforeSend: function(request) { request.setRequestHeader("Authorization", 'Bearer ' + access_token); },
+      url: "https://api.fitbit.com/1/user/"+userId+"/activities/tracker/calories/date/today/1d.json",
+      success:function(data, status, xhr){
+        G.tracker_bmr_calories = parseInt(data['activities-tracker-calories'][0]['value']);
         }
-    }).done(function(result) {
-        /* do something with the result here */
-        result = result['activities-tracker-calories'][0]['value'];
-        callback(result); // invokes the callback function passed as parameter
-    });
+    })
 
-}
+)
+    .then( myFunc, myFailure );
 
-yourFunction(function(result) {
-    console.log('Result! ', result);
-});
-
-// $.ajax({ //activities/tracker/calories - Calories burned inclusive of BMR according to movement captured by a Fitbit tracker.
-//   type: 'GET',
-//   beforeSend: function(request) {
-//      request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-//   },
-//     url: "https://api.fitbit.com/1/user/"+userId+"/activities/tracker/calories/date/today/1d.json",
-//     success:function(data, status, xhr){
-//     	var trackerCalories = data['activities-tracker-calories'][0]['value'];
-//       console.log('trackerCalories = ' + trackerCalories);
-//       $('#calories-left').html(trackerCalories);
-//     }
-// });
-
-// $.ajax({ // activities/caloriesBMR - Only BMR calories.
-//   type: 'GET',
-//   beforeSend: function(request) {
-//      request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-//   },
-//     url: "https://api.fitbit.com/1/user/"+userId+"/activities/caloriesBMR/date/"+yesterday+"/1d.json",
-//     success:function(data, status, xhr){
-//     	caloriesBMR = data['activities-caloriesBMR'][0]['value'];
-//       //console.log(data['activities-tracker-calories'][0]['value']);
-//       console.log('caloriesBMR = ' + caloriesBMR);
-//       //$('#calories-left').html(myNumber);
-//     }
-// });
-//
-// $.ajax({ //activities/activityCalories - The number of calories burned during the day for periods of time when the user was active above sedentary level. This value is calculated minute by minute for minutes that fall under this criteria. This includes BMR for those minutes as well as activity burned calories.
-//   type: 'GET',
-//   beforeSend: function(request) {
-//      request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-//   },
-//     url: "https://api.fitbit.com/1/user/"+userId+"/activities/activityCalories/date/today/1d.json",
-//     success:function(data, status, xhr){
-//     	var activityCalories = data['activities-activityCalories'][0]['value'];
-//       console.log('activityCalories = ' + activityCalories);
-//     }
-// });
-//
-// $.ajax({ // activities/caloriesBMR - Only BMR calories.
-//   type: 'GET',
-//   beforeSend: function(request) {
-//      request.setRequestHeader("Authorization", 'Bearer ' + access_token);
-//   },
-//     url: "https://api.fitbit.com/1/user/"+userId+"/activities/caloriesBMR/date/today/1w.json",
-//     success:function(data, status, xhr){
-//     	//var caloriesBMR = data['activities-caloriesBMR'][0]['value'];
-//       //console.log(data['activities-tracker-calories'][0]['value']);
-//       //console.log('caloriesBMR = ' + caloriesBMR);
-//       //$('#calories-left').html(myNumber);
-//       console.log(data);
-//     }
-// });
-//
-// console.log(caloriesBMR);
-//
-//
-// var bob = myClosure();
-// console.log(bob);
-
-
-/*
-
-
-function celebrityName (firstName) {
-    var nameIntro = "This celebrity is ";
-    // this inner function has access to the outer function's variables, including the parameter​
-   function lastName (theLastName) {
-        return nameIntro + firstName + " " + theLastName;
+    function myFunc() {
+      //console.log (G.BMR_by_minute);
+      G.remaining_BMR_calories = G.minsToMidnight * G.BMR_by_minute;
+      G.BMR_calories_so_far = G.BMR_total_yesterday - G.remaining_BMR_calories;
+      console.log(G);
+      console.log(G.BMR_calories_so_far);
+      G.projected_burn = parseInt(G.tracker_bmr_logged_calories + G.remaining_BMR_calories);
+      //$('#calories-consumed').html('Pace: ' + G.projected_burn);
+      $('#calories-in').html(G.caloriesIn);
+      $('#projected-burn').html(G.projected_burn);
+      $('#calorie-deficit').html(G.projected_burn - G.caloriesIn);
+      //$('#calories-left').html('Rem: ' + parseInt(G.remaining_BMR_calories));
+      //$('#calories-out').html('Curr Out: ' + G.tracker_bmr_logged_calories);
     }
-    return lastName;
-}
-​
-​var mjName = celebrityName ("Michael"); // At this juncture, the celebrityName outer function has returned.​
-​
-​// The closure (lastName) is called here after the outer function has returned above​
-​// Yet, the closure still has access to the outer function's variables and parameter​
-mjName ("Jackson"); // This celebrity is Michael Jackson 
 
-
-
-
-Calorie Time Series Differences
-
-activities/calories - The top level time series for calories burned inclusive of BMR, tracked activity, and manually logged activities.
-
-activities/caloriesBMR - Only BMR calories.
-
-activities/activityCalories - The number of calories burned during the day for periods of time when the user was active above sedentary level. This value is calculated minute by minute for minutes that fall under this criteria. This includes BMR for those minutes as well as activity burned calories.
-
-activities/tracker/calories - Calories burned inclusive of BMR according to movement captured by a Fitbit tracker.
-
-activities/tracker/activityCalories - Calculated similarly to activities/activityCalories, but uses only tracker data. This means that manually logged activities are excluded.
-*/
+    function myFailure() {
+      console.log('failure');
+    }
