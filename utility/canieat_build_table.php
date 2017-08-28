ef1a101e9057fc096d6d1d22b07df35ce62a20b4
Dan Fitzpatrick <dan.fpatrick@gmail.com>
2017-08-27 18:10:33 -0700

    Create utility file to build canieat db table

---
+++ utility/canieat_build_table.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "mysql.canieat.danfitzpatrick.net";
+$username = "canieat";
+$password = "e-eats-everything";
+$dbname = "canieat";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE canieat_log (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+cal_eaten INT(6) NOT NULL,
+time_logged TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table canieat_log created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
